f09f2d075f0e3ccd9ee5ab7d3114d230a9a43b3e
Antônio Luíz <juniorrluiz.contato@gmail.com>
2021-04-08 23:51:12 -0300

    loop

--- Curso_Java_2/src/br/cursojava_atividades/Decima_Terceira_Questao.java
+++ Curso_Java_2/src/br/cursojava_atividades/Decima_Terceira_Questao.java
@@ -13,44 +13,107 @@ public class Decima_Terceira_Questao {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Insira o dia da semana para: "
+		System.out.println("\n Insira o dia da semana para: "
 				+ "\n (1) Domingo, (2) Segunda,"
 				+ "(3) Terça (4) Quarta, (5) Quinta,"
 				+ "(6) Sexta e (7) Sabado ");
-		
+
 		int diaSemana = sc.nextInt();
-		
-		switch (diaSemana) {
-		case 1:
-			System.out.println("Domingo");
-			break;
-		case 2:
-			System.out.println("Segunda");
-			break;
-		case 3:
-			System.out.println("Terça");
-			break;
-		case 4:
-			System.out.println("Quarta");
-			break;
-		case 5:
-			System.out.println("Quinta");
-			break;
-		case 6:
-			System.out.println("Sexta");
-			break;
-		case 7:
-			System.out.println("Sábado");
-			break;
+		boolean valida = false;
 
-		default:
-			System.out.println("Opção invalida");
-			break;
-		}
-		
+		do {
+
+			//f (diaSemana >= 1 && diaSemana <= 7) {
 
 
-		sc.close();
-	}
+			switch (diaSemana) {
+			case 1:
+				System.out.println("Domingo");
+				valida = true;
+				break;
+			case 2:
+				System.out.println("Segunda");
+				valida = true;
+				break;
+			case 3:
+				System.out.println("Terça");
+				valida = true;
+				break;
+			case 4:
+				System.out.println("Quarta");
+				valida = true;
+				break;
+			case 5:
+				System.out.println("Quinta");
+				valida = true;
+				break;
+			case 6:
+				System.out.println("Sexta");
+				valida = true;
+				break;
+			case 7:
+				System.out.println("Sábado");
+				valida = true;
+				break;
+			default:
+				System.out.println("Opção invalida, tente novamente");
+				diaSemana = sc.nextInt();
+				valida = false;
+
+				break;
+			} 
+
+		} while(!valida);
+
+
+
+		/*do {
+
+
+
+
+
+			if (diaSemana == 1) {
+				System.out.println("Domingo");
+				valida = true;
+
+			} else if (diaSemana == 2) {
+				System.out.println("Segunda");
+				valida = true;
+
+			} else if (diaSemana == 3) {
+				System.out.println("Terça");
+				valida = true;
+
+			} else if (diaSemana == 4) {
+				System.out.println("Quarta");
+				valida = true;
+
+			} else if (diaSemana == 5) {
+				System.out.println("Quinta");
+				valida = true;
+
+			} else if (diaSemana == 6) {
+				System.out.println("Sexta");
+				valida = true;
+
+			} else if (diaSemana == 7) {
+				System.out.println("Sábado");
+				valida = true;
+
+			} else {
+				System.out.println("Opção inválida, tente novamente");
+				valida = false;
+			}
+
+		} while (!valida);
+
+
+		valida = true;
+		 */
+
+	} 
 
 }
+
+
