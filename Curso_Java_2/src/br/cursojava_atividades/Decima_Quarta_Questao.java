ac1dc789ba080bfb56ec6992fe458a5e95b3a430
Antônio Luíz <juniorrluiz.contato@gmail.com>
2021-04-13 22:08:28 -0300

    Add questão

---
+++ Curso_Java_2/src/br/cursojava_atividades/Decima_Quarta_Questao.java
@@ -0,0 +1,22 @@
+package br.cursojava_atividades;
+
+import java.util.Scanner;
+
+
+/*13.Fa�a um Programa que leia um n�mero e exiba o dia correspondente 
+da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve 
+aparecer valor inv�lido.*/
+
+
+public class Decima_Quarta_Questao {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		
+
+	} 
+
+}
+
+
